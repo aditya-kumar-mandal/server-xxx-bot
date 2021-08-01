d241ce60249d84f32bcb089601474367fa853baa
akm-akm <adityanick08@gmail.com>
2021-08-02 01:55:01 +0530

    ghhghh

--- index.js
+++ index.js
@@ -3,17 +3,52 @@ const server = new express();
 const port = process.env.PORT || 3333;
 const fs = require('fs');
 const path = require("path");
+const mongoose = require('mongoose');
+const node_cron = require('node-cron');
+mongo_uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/x';
+mongoose.connect(mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const Botdata = mongoose.model('botlist', {
+    ownernumber: {
+        type: String,
+    },
+    botnumber: {
+        type: String,
+    },
+    bot_url: {
+        type: String,
+    }
+})
 const abuse = JSON.parse(
     fs.readFileSync(path.join(__dirname, "./abuse.json"))
 );
 const cron = require('node-cron');
 
-console.clear();
+//console.clear();
 
+node_cron.schedule('0 0 * * *',  ()=> {
+    Botdata.find({}).exec(function (err, botdata) {
+        if (err) {
+            console.log(err);
+        } else {
+            botdata.forEach(currentItem => {
+                axios.get(currentItem.bot_url + '/resetdailycount').then(function (response) {
+                    console.log(response.data);
+                }
+                ).catch(function (error) {
+                    console.log(error);
+                })
+            });
+        }
+    });
+});
 
+          
 
 server.listen(port, () => {
-    console.clear();
+   
     console.log("\nRunnning on http://localhost:" + port);
 });
 
@@ -21,10 +56,11 @@ server.listen(port, () => {
 server.use(
     express.urlencoded({
         extended: true,
-    })
+    }),
+    express.json()
 );
 
-
+server.use(express.json())
 server.get("/", (req, res) => {
     console.log("/");
     res.send("XXX-Bot server");
@@ -49,14 +85,28 @@ server.get("/isabuse/:id", async (req, res) => {
 });
 
 
-server.get("/register_new_bot/:id", async (req, res) => {
+server.post("/register_new_bot", async (req, res) => {
     console.log("register-new-bot");
-    console.log(req.params.id);
-    datainsert({ "Name": req.params.id },"botlist").then(() => {
-        res.send("OK");
+    console.log(req.body);
+    const mongo = new Botdata(req.body)
+    mongo.save().then(() => {
+        res.status(201).send("OK");
+    }).catch(e => {
+        res.status(500).send(e);
     }
+    );
+})
 
-    )
+server.post("/is_registered", async (req, res) => {
+    console.log("is_registered");
+    Botdata.find({ 'bot_url': req.body.bot_url }).then((result) => {
+        console.log('find result-', result.length);
+        result.length ?  res.send(true): res.send(false);
+    }
+    ).catch(e => {
+        console.log('find error', e);
+    }
+    );
 })
 
 process.on('uncaughtException', err => console.log(err));
--- mongo.js
+++
@@ -1,3 +0,0 @@
-s ="hi. dick.54 xxx, FUCK ,chutiye, lund."
-word_array = s.replace(/[^a-z]/gmi, " ").replace(/\s+/g, " ").split(" ")
-console.log(word_array);
